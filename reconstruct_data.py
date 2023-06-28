9834ebfdebefb0a06cca932da2b32c53592c09ce
BrianPark314 <everfrost314@gmail.com>
2023-06-28 15:43:45 +0900

    yaml support added

--- reconstruct_data.py
+++ reconstruct_data.py
@@ -1,5 +1,5 @@
 from common.params import args
-from common.utils import create_dirs, unzip, parse_json, move_image, create_label_files
+from common.utils import create_dirs, unzip, parse_json, move_image, create_label_files, get_yaml
 from common.image import get_contour, resize_image
 if __name__ == '__main__':
     train_path = args.data_path / 'train/'
@@ -10,5 +10,6 @@ if __name__ == '__main__':
     #parse_json(train_path)
     #move_image(train_path)
     #create_label_files(train_path)
-    resize_image(train_path / 'images/')
+    #resize_image(train_path / 'images/')
+    get_yaml(args.data_path)
     #get_contour(train_path / 'images/')
