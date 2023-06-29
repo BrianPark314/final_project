f3ccb2652dc1e344cf3e8c85ced39caba824b96e
ejr3949@naver.com <ejr3949@naver.com>
2023-06-29 09:53:00 +0900

    캡슐 및 알약 구분(사각형그리기)을 위한 분기 작성. cv2.dilate 함수추가

--- common/image.py
+++ common/image.py
@@ -22,11 +22,22 @@ def get_contour(path):
     result = []  # 종류 + 좌표 담는 리스트 
     for i, img_path in enumerate(path_list): # file_list_png는 png데이터가 있는 파일 
         img = cv2.imread(str(img_path)) # 읽기
-
+        img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
         gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #gray
-        blurred_img = cv2.GaussianBlur(gray_img, (5, 5),0) # blur처리
-        ret, otsu = cv2.threshold(blurred_img,-1,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU) #otsu최적화 
-        contours , hierarchy = cv2.findContours(otsu,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE) #윤곽추출
+        blur = cv2.GaussianBlur(gray_img, ksize=(5,5), sigmaX=0)
+        edged = cv2.Canny(blur, 10, 100)
+        
+        ##아래의 if문은 json처리가 완료되면 진행 예정
+        if : #json에 캡슐이들어가는 컬럼을통해 분기를 태워서 캡슐과 일반 약품 사각형처리의 차이를둠
+            ret, otsu = cv2.threshold(edged,-1,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU) #otsu최적화 
+            contours , hierarchy = cv2.findContours(otsu,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE) #윤곽추출
+        elif : #여기 부분이 캡슐일때 처리하는곳
+            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (11,11))
+            result = cv2.dilate(edged, kernel, iterations = 3)
+            closed = cv2.morphologyEx(result, cv2.MORPH_CLOSE, kernel)
+            ret, thresh = cv2.threshold(closed,127,255,0)
+            contours, hierachy = cv2.findContours(thresh, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
         test_img = cv2.drawContours(img, contours, 0, 2)
         show_img(img, contours)
         x, y, w, h = cv2.boundingRect(np.array(contours[1])) # 딱 맞는 사각형 좌표 
