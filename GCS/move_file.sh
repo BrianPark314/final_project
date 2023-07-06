34ccdf7254b58d6d1ab710f9d0b84bfcb6cd4853
BrianPark314 <everfrost314@gmail.com>
2023-07-06 15:13:35 +0900

    unzip with bash shell test #19

--- GCS/move_file.sh
+++ GCS/move_file.sh
@@ -3,3 +3,6 @@
 cd /content/data/unzip
 
 find . -name '*.png' -exec mv {} /content/data/train/images \;
+
+gsutil cat gs://pill_data_brain/zip/TS_단일_42.zip | for i in $(jar --list); do gsutil cat gs://pill_data_brain/zip/TS_단일_42.zip | jar x $i && cat $i | gsutil cp - gs://pill_data_brain/unzip/$i && rm ./$i; done;
+gsutil -m cat gs://pill_data_brain/zip/TS_42_단일.zip
--- GCS/unzip.sh
+++ GCS/unzip.sh
@@ -3,5 +3,6 @@
 cd /content/data/zip/
 
 for file in *.zip; do
+  echo ${file}
   unzip "${file}" -d "/content/data/unzip" && rm "${file}"
 done
