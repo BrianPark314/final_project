cbd74017efe8c0729071aac2b61761a08c083744
BrianPark314 <everfrost314@gmail.com>
2023-07-07 15:00:53 +0900

    automated pipeline implemented #19

--- GCS/move_file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /content/data/unzip
-
-find . -name '*.png' -exec mv {} /content/data/train/images \;
-
--- common/params.py
+++ common/params.py
@@ -5,3 +5,4 @@ import os
 args = easydict.EasyDict()
 args.base_path = Path(os.getcwd())
 args.data_path = Path('/content/data')
+args.script_path = args.base_path / 'GCS/scripts/'
