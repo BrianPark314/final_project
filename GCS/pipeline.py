f6e41d8a08131df0aafd270d9904036c845785e8
brian <everfrost314@gmail.com>
2023-07-10 09:57:40 +0900

    added logging to pipelin

--- GCS/pipeline.py
+++ GCS/pipeline.py
@@ -1,6 +1,6 @@
 import subprocess
 import sys
-sys.path.insert(0, '/Users/Shark/Projects/final_project/')
+sys.path.insert(0, '/home/brian/repos/final_project')
 import time
 import os
 
@@ -14,5 +14,6 @@ if __name__ == '__main__':
             print('No zip file download complete.')
         subprocess.run(args.script_path / 'upload.sh')
         subprocess.run(args.script_path / 'clean.sh')  
-        #print(os.environ['file'])
         time.sleep(60)      
+    # subprocess.run(args.script_path / 'test.sh')
+    # print(os.environ['file'])
